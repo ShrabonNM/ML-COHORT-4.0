ebb6a6428cf0adc71540d582135c8c92b4415cda
Noor Mohammed Shrabon <noormohammedshrabon@gmail.com>
2025-03-25 10:45:06 +0600

    Add files via upload

---
+++ Tuple Task 2 py.py
@@ -0,0 +1,8 @@
+
+cities = ("paris", "london", "new-york", "toronto", "dubai")
+
+print(cities[2])  
+new_data = list(cities)
+new_data.append("washington")
+new_new_data = tuple(new_data)
+print(new_new_data)
