a4d553b3e5229daf0e6c7851b3d63b60ab26099a
Noor Mohammed Shrabon <noormohammedshrabon@gmail.com>
2025-03-25 09:55:37 +0600

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,7 @@
+person = {"name": "john"}  # Correct dictionary initialization
+person["age"] = 30         # Assigning age to the dictionary
+person["name"] = "john doe"  # Updating the name key
+del person["age"]  # Deleting the age key
+
+print(person)  # Output: {'name': 'john doe'}
+
---
+++ list and tuple .py
@@ -0,0 +1,9 @@
+data=[20,30,49,99,78]
+data.append(56)   #remove an item
+data.remove(30) #remove an item
+print(data)
+
+pata=(20,30,49,99,780)
+new_data=list(pata)
+new_data.append(56)   
+new_data.remove(30)
