20a5902de86391955e787c80de744d1a23b8feee
Noor Mohammed Shrabon <noormohammedshrabon@gmail.com>
2025-04-03 11:38:40 +0600

    Add files via upload

---
+++ function,py.py
@@ -0,0 +1,13 @@
+def my_function():
+    print("hello from a function")
+my_function()
+
+def my_function2(Fname,Mname,Lname):
+    print(Fname +" " + Mname + " " +Lname)
+my_function2("Noor","Mohammed","Shrabon")
+
+
+#NID CARD
+def NID_function(Name,Date_of_Birth,Gender,Present_Address,Permanent_Address):
+    print(Name + " " + Date_of_Birth + " " + Gender + " " + Present_Address + " " + Permanent_Address )
+NID_function("Noor Mohammed Shrabon","2nd september 2005","Male","Dhaka","Cumilla")
